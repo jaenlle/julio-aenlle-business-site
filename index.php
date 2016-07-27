34c49782425255810d82eb9378878de5bd869298
Julio Aenlle <gmg8r251@ufl.edu>
2016-07-27 01:14:43 -0400

    Add index content & styling

--- index.php
+++ index.php
@@ -2,35 +2,31 @@
 
     <!-- Begin Body Content Section -->
     <section class="row">
-        <div class="eight columns">
-
-            <!-- Begin Loop -->
-
-            <?php
-                if ( have_posts() ){
-                    while ( have_posts() ){
-                        the_post(); ?>
-                            <h1><a href="<?php the_permalink(); ?>"><?php the_title(); //this php calls up each post title ?></a></h1>
-                            <!-- Blacked timestamp <p style="color:orange;"><?php the_time('F j, Y'); ?></p>-->
-                            <?php
-                            if ( has_post_thumbnail() ) {
-                                $image_src = wp_get_attachment_image_src( get_post_thumbnail_id(),’thumbnail’ );
-                                echo '<img width="100%" src="' . $image_src[0] . '">';//ideal image width is 627; scale accordingly
-                            } ?>
-                            <?php the_excerpt(); //this pulls a paragraph of content from each post ?>
-                        <p class="readmore"><a href="<?php the_permalink(); ?>">Read More</a></p>
-                    <?php } //end while statement
-                } //end if statement
-              ?>
+        <div class="twelve columns">
+            <div class="placeholder"></div>
         </div>
-        <!-- End Loop -->
-
-        <div class="one column" id="blank">blank</div>
-
-        <!-- ADDING SIDEBAR -->
-        <div class="three columns">
-            <?php get_sidebar(); ?>
+        <div class="two columns blank">Blank Column</div>
+        <div class="eight columns main">
+            <h1>Our Promise To You</h1>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent mollis vitae enim sed finibus. Aenean tincidunt est ut arcu vulputate porta. Aenean tellus leo, malesuada et nulla ac, ullamcorper convallis enim. Phasellus bibendum, erat in eleifend porta, erat purus dictum lectus, sit amet cursus ante augue sed quam. Morbi ac nisi vel tellus lacinia commodo eget a enim. Vestibulum volutpat velit quis hendrerit hendrerit. Maecenas eu fermentum urna. In rutrum arcu eu facilisis rhoncus. Vestibulum sit amet malesuada nibh. Praesent at turpis at velit rhoncus auctor.
+            </p>
+            <a href="#">MAKE AN APPOINTMENT</a>
         </div>
+        <div class="two columns blank">Black Column</div>
+        <div class="twelve columns maindeals">
+            <ul>
+                <li><div class="blocks"><a href="#">10% OFF</a>
+                </div></li>
+                <li><div class="blocks"><a href="#">Buy 1, Get 1</a>
+                </div></li>
+                <li><div class="blocks"><a href="#">FREE</a>
+                </div></li>
+                <li><div class="blocks"><a href="#">25% OFF</a>
+                </div></li>
+            </ul>
+        </div>
+
     </section> <!-- End Body Content Section -->
 
 <?php get_footer(); ?> <!-- Gets information from footer.php to display on page -->
