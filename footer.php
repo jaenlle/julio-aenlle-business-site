731dc5ae3fe96dab8c3a527989e56b80295868df
Julio Aenlle <gmg8r251@ufl.edu>
2016-07-26 23:43:27 -0400

    Modify footer html & styling

--- footer.php
+++ footer.php
@@ -1,13 +1,30 @@
       <footer class="row">
             <div class="twelve columns">
                 <div class="row"> <!-- this div houses the menu -->
-                    <div class="twelve columns">
-                        <div id="footernav">
+                    <div class="four columns">
+                        <div class="footernav">
                             <ul>
-                                <li></li>
-                                <li><a href=""></a></li>
-                                <li><a href=""></a></li>
+                                <li style="color: #f5ff6e; font-size: 1.4em">Navigation</li>
+                                <li><a href="">HOME</a></li>
+                                <li><a href="">SERVICES</a></li>
+                                <li><a href="">PROMOTIONS</a></li>
+                                <li><a href="">BLOG</a></li>
                             </ul>
+                        </div>
+                    </div>
+                    <div class="four columns">
+                        <div class="footernav">
+                            <ul>
+                                <li style="color: #f5ff6e; font-size: 1.4em">Contact</li>
+                                <li><a href="mailto:info@slicksautobody.net">EMAIL US</a></li>
+                                <li>1-866-555-0795</li>
+                                <li><a href="/slicksautobody/page_contactform.php">Find a location</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="four columns">
+                        <div class="footerlogo">
+                            <img src="http://julioaenlle.net/slicksautobody/wp-content/uploads/2016/07/logo_yellow.png" alt="Slick's Autobody" />
                         </div>
                     </div>
                 </div>
