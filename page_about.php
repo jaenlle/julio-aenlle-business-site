3a240dbe09f6a4625efdcb982a96054f56448986
Julio Aenlle <gmg8r251@ufl.edu>
2016-07-27 21:58:07 -0400

    Create and style about page

--- page_about.php
+++ page_about.php
@@ -3,3 +3,41 @@
 Template Name: About Page
 */
 ?>
+
+<?php get_header(); ?> <!-- Gets information from header.php to display on page -->
+    <section class="row">
+        <div class="twelve columns story">
+            <h1>Our Story</h1>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent mollis vitae enim sed finibus. Aenean tincidunt est ut arcu vulputate porta. Aenean tellus leo, malesuada et nulla ac, ullamcorper convallis enim. Phasellus bibendum, erat in eleifend porta, erat purus dictum lectus, sit amet cursus ante augue sed quam. Morbi ac nisi vel tellus lacinia commodo eget a enim. Vestibulum volutpat velit quis hendrerit hendrerit. Maecenas eu fermentum urna. In rutrum arcu eu facilisis rhoncus. Vestibulum sit amet malesuada nibh. Praesent at turpis at velit rhoncus auctor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent mollis vitae enim sed finibus. Aenean tincidunt est ut arcu vulputate porta. Aenean tellus leo, malesuada et nulla ac, ullamcorper convallis enim. Phasellus bibendum, erat in eleifend porta, erat purus dictum lectus, sit amet cursus ante augue sed quam.
+            </p>
+        </div>
+        <div class="four columns doe">
+            <div id="doe1">
+            </div>
+            <h1>John "Slick" Doe</h1>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent mollis vitae enim sed finibus. Aenean tincidunt est ut arcu vulputate porta. Aenean tellus leo, malesuada et nulla ac, ullamcorper convallis enim. Phasellus bibendum, erat in eleifend porta, erat purus dictum lectus, sit amet cursus ante augue sed quam. Morbi ac nisi vel tellus lacinia commodo eget a enim. Vestibulum volutpat velit quis hendrerit hendrerit. Maecenas eu fermentum urna. In rutrum arcu eu facilisis rhoncus.
+            </p>
+        </div>
+        <div class="four columns doe">
+            <div id="doe2">
+            </div>
+            <h1>Joe Doe</h1>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent mollis vitae enim sed finibus. Aenean tincidunt est ut arcu vulputate porta. Aenean tellus leo, malesuada et nulla ac, ullamcorper convallis enim. Phasellus bibendum, erat in eleifend porta, erat purus dictum lectus, sit amet cursus ante augue sed quam. Morbi ac nisi vel tellus lacinia commodo eget a enim. Vestibulum volutpat velit quis hendrerit hendrerit. Maecenas eu fermentum urna. In rutrum arcu eu facilisis rhoncus.
+            </p>
+        </div>
+        <div class="four columns doe">
+            <div id="doe3">
+            </div>
+            <h1>Bo Doe-Tho</h1>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent mollis vitae enim sed finibus. Aenean tincidunt est ut arcu vulputate porta. Aenean tellus leo, malesuada et nulla ac, ullamcorper convallis enim. Phasellus bibendum, erat in eleifend porta, erat purus dictum lectus, sit amet cursus ante augue sed quam. Morbi ac nisi vel tellus lacinia commodo eget a enim. Vestibulum volutpat velit quis hendrerit hendrerit. Maecenas eu fermentum urna. In rutrum arcu eu facilisis rhoncus.
+            </p>
+        </div>
+        <div class="twelve columns appt">
+            <a href="#">MAKE AN APPOINTMENT</a>
+        </div>
+    </section>
+<?php get_footer(); ?> <!-- Gets information from footer.php to display on page -->
