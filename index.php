639a1edc896376c9fc1f84cbb1e2a45852d54278
Julio Aenlle <gmg8r251@ufl.edu>
2016-07-29 21:44:19 -0400

    Add link to make appt. button

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
             <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent mollis vitae enim sed finibus. Aenean tincidunt est ut arcu vulputate porta. Aenean tellus leo, malesuada et nulla ac, ullamcorper convallis enim. Phasellus bibendum, erat in eleifend porta, erat purus dictum lectus, sit amet cursus ante augue sed quam. Morbi ac nisi vel tellus lacinia commodo eget a enim. Vestibulum volutpat velit quis hendrerit hendrerit. Maecenas eu fermentum urna. In rutrum arcu eu facilisis rhoncus. Vestibulum sit amet malesuada nibh. Praesent at turpis at velit rhoncus auctor.
             </p>
-            <a href="#">MAKE AN APPOINTMENT</a>
+            <a href="http://julioaenlle.net/slicksautobody/index.php/contact/">MAKE AN APPOINTMENT</a>
         </div>
         <div class="two columns blank">Black Column</div>
         <div class="twelve columns maindeals">
