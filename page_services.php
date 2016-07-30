97681c9c762f98884cf8c6a38e3c4e4bd984ebd0
Julio Aenlle <gmg8r251@ufl.edu>
2016-07-30 08:37:08 -0400

    Add services page content & styling

--- page_services.php
+++ page_services.php
@@ -3,3 +3,44 @@
 Template Name: Services Page
 */
 ?>
+    <?php get_header(); ?>
+        <div class="one column blank">Blank Column</div>
+        <div class="row">
+            <div class="ten columns services">
+                <h1>Services</h1>
+                <div class="row servetop">
+                    <div class="servleft sl1">
+                        <img class="mech" src="http://julioaenlle.net/slicksautobody/wp-content/uploads/2016/07/mech2.jpg" alt="" />
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi mattis vel mauris in vulputate. Cras sem ante, mollis quis accumsan vitae, suscipit vitae lectus. Sed eget semper ligula. Vivamus efficitur, odio vitae congue rhoncus, dui purus maximus nibh, sed suscipit lacus urna a purus. Vivamus sit amet eros arcu. Ut condimentum, nisi eu bibendum rutrum, nibh purus aliquet elit, et malesuada velit lacus eu magna. Aliquam eu magna elit.
+                        </p>
+                        <img class="icon" src="http://julioaenlle.net/slicksautobody/wp-content/uploads/2016/07/icon-performance.png" alt="" />
+                    </div>
+                    <div class="servright">
+                        <img class="mech" src="http://julioaenlle.net/slicksautobody/wp-content/uploads/2016/07/mech1.jpg" alt="" />
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi mattis vel mauris in vulputate. Cras sem ante, mollis quis accumsan vitae, suscipit vitae lectus. Sed eget semper ligula. Vivamus efficitur, odio vitae congue rhoncus, dui purus maximus nibh, sed suscipit lacus urna a purus. Vivamus sit amet eros arcu. Ut condimentum, nisi eu bibendum rutrum, nibh purus aliquet elit, et malesuada velit lacus eu magna. Aliquam eu magna elit.
+                        </p>
+                        <img class="icon" src="http://julioaenlle.net/slicksautobody/wp-content/uploads/2016/07/icon-tire.png" alt="" />
+                    </div>
+                </div>
+                <div class="row servebottom">
+                    <div class="servleft">
+                        <img class="mech" src="http://julioaenlle.net/slicksautobody/wp-content/uploads/2016/07/mech3.jpg" alt="" />
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi mattis vel mauris in vulputate. Cras sem ante, mollis quis accumsan vitae, suscipit vitae lectus. Sed eget semper ligula. Vivamus efficitur, odio vitae congue rhoncus, dui purus maximus nibh, sed suscipit lacus urna a purus. Vivamus sit amet eros arcu. Ut condimentum, nisi eu bibendum rutrum, nibh purus aliquet elit, et malesuada velit lacus eu magna. Aliquam eu magna elit.
+                        </p>
+                        <img class="icon" src="http://julioaenlle.net/slicksautobody/wp-content/uploads/2016/07/icon-maintenance.png" alt="" />
+                    </div>
+                    <div class="servright">
+                        <img class="mech" src="http://julioaenlle.net/slicksautobody/wp-content/uploads/2016/07/mech4.jpg" alt="" />
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi mattis vel mauris in vulputate. Cras sem ante, mollis quis accumsan vitae, suscipit vitae lectus. Sed eget semper ligula. Vivamus efficitur, odio vitae congue rhoncus, dui purus maximus nibh, sed suscipit lacus urna a purus. Vivamus sit amet eros arcu. Ut condimentum, nisi eu bibendum rutrum, nibh purus aliquet elit, et malesuada velit lacus eu magna. Aliquam eu magna elit.
+                        </p>
+                        <img class="icon" src="http://julioaenlle.net/slicksautobody/wp-content/uploads/2016/07/icon-repair.png" alt="" />
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="one column blank">Blank Column</div>
+    <?php get_footer(); ?>
