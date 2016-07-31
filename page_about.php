12e974f7b2b4633698269262e7f75a292d7f8634
Julio Aenlle <gmg8r251@ufl.edu>
2016-07-30 21:46:45 -0400

    Add paragraph content to about page

--- page_about.php
+++ page_about.php
@@ -9,7 +9,7 @@ Template Name: About Page
         <div class="twelve columns story">
             <h1>Our Story</h1>
             <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent mollis vitae enim sed finibus. Aenean tincidunt est ut arcu vulputate porta. Aenean tellus leo, malesuada et nulla ac, ullamcorper convallis enim. Phasellus bibendum, erat in eleifend porta, erat purus dictum lectus, sit amet cursus ante augue sed quam. Morbi ac nisi vel tellus lacinia commodo eget a enim. Vestibulum volutpat velit quis hendrerit hendrerit. Maecenas eu fermentum urna. In rutrum arcu eu facilisis rhoncus. Vestibulum sit amet malesuada nibh. Praesent at turpis at velit rhoncus auctor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent mollis vitae enim sed finibus. Aenean tincidunt est ut arcu vulputate porta. Aenean tellus leo, malesuada et nulla ac, ullamcorper convallis enim. Phasellus bibendum, erat in eleifend porta, erat purus dictum lectus, sit amet cursus ante augue sed quam.
+                Slick's was begun in a garage in West Dade County, Fl back when the Everglades reached much farther east than they do now. After just a few years John "Slick" Doe, seen below, built up a strong reputation as good, fast worker who could turn a car around in less than a hour. Pretty soon John was opening his first shop in a small former utility building behind a well-traversed strip mall. Eventually the shop became successful enough to move to its first permanent location in Doral, FL near Miami International Mall. The shop eventually grew to employ over 20 full-time full-service mechanics and several other specialists. After Slick's brother Joe left the United States Navy business boomed and eventually Joe opened a second spot along U.S. 1. Slick's has numerous awards from the city of Miami and the county, and was even featured in the short-lived TLC show " Americanics". Just last year Slick's expanded into Broward County, opening its third location in Weston, Fl. 
             </p>
         </div>
         <div class="four columns doe">
@@ -17,7 +17,7 @@ Template Name: About Page
             </div>
             <h1>John "Slick" Doe</h1>
             <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent mollis vitae enim sed finibus. Aenean tincidunt est ut arcu vulputate porta. Aenean tellus leo, malesuada et nulla ac, ullamcorper convallis enim. Phasellus bibendum, erat in eleifend porta, erat purus dictum lectus, sit amet cursus ante augue sed quam. Morbi ac nisi vel tellus lacinia commodo eget a enim. Vestibulum volutpat velit quis hendrerit hendrerit. Maecenas eu fermentum urna. In rutrum arcu eu facilisis rhoncus.
+                The Slick on the sign above the door, John Doe has had cars on the brain as long as he can remember. He first souped up his dad's riding mower to hit 65 mph before police told him he was too young to go to jail. But he's more than a grease monkey. When he's not working on vehicles at the shop or cars in his home garage he's likely to be sitting under a tree reading some of history's greatest philosophers, his favorite being Descartes (and not because it rhymes with gokarts).
             </p>
         </div>
         <div class="four columns doe">
@@ -25,7 +25,7 @@ Template Name: About Page
             </div>
             <h1>Joe Doe</h1>
             <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent mollis vitae enim sed finibus. Aenean tincidunt est ut arcu vulputate porta. Aenean tellus leo, malesuada et nulla ac, ullamcorper convallis enim. Phasellus bibendum, erat in eleifend porta, erat purus dictum lectus, sit amet cursus ante augue sed quam. Morbi ac nisi vel tellus lacinia commodo eget a enim. Vestibulum volutpat velit quis hendrerit hendrerit. Maecenas eu fermentum urna. In rutrum arcu eu facilisis rhoncus.
+                Joe Doe, brother to John, joined his brother as co-owner after a successful stint as a mechanic in the United States Navy, so when he tells clients that he's seen everything that can possibly happen to an engine he's not kidding. Fun Fact: Joe is an expert knitter, a self-taught skill perfect over many years spent at sea. He's made all of our mechanics' uniforms (that's actually not true because that would be brutal to work in).
             </p>
         </div>
         <div class="four columns doe">
@@ -33,7 +33,7 @@ Template Name: About Page
             </div>
             <h1>Bo Doe-Tho</h1>
             <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent mollis vitae enim sed finibus. Aenean tincidunt est ut arcu vulputate porta. Aenean tellus leo, malesuada et nulla ac, ullamcorper convallis enim. Phasellus bibendum, erat in eleifend porta, erat purus dictum lectus, sit amet cursus ante augue sed quam. Morbi ac nisi vel tellus lacinia commodo eget a enim. Vestibulum volutpat velit quis hendrerit hendrerit. Maecenas eu fermentum urna. In rutrum arcu eu facilisis rhoncus.
+                Bo is the black sheep of the family. Bo is the shops resident motorcycle expert after having spent many a year in a hardcore biker gang. Has he killed a man before? The tattoo on his neck says yes but no one's really ever asked him. Some things are just better left unsaid. But we swear Bo's the sweetest guy in the shop. He's also our most accomplished athlete: starting pitcher on the defending two-time South Florida Mechanics League champion Slick's softball team.
             </p>
         </div>
         <div class="twelve columns appt">
