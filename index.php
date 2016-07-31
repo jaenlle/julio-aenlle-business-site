c1312365a79669c4e40d64739f3e5925180ac986
Julio Aenlle <gmg8r251@ufl.edu>
2016-07-31 19:48:32 -0400

    Modify main section to ten columns

--- index.php
+++ index.php
@@ -34,15 +34,14 @@
                 </ul>
             </div>
         </div>
-        <div class="two columns blank">Blank Column</div>
-        <div class="eight columns main">
+        <div class="one columns blank">Blank Column</div>
+        <div class="ten columns main">
             <h1>Our Promise To You</h1>
             <p>
                 Throughout our careers we've heard stories from countless customers about how they've been tricked, swindled, hustled, bamboozled, conned and overall just treated not very nicely by other mechanics not only her in South Florida, but around the country. And we think that's a darned shame because our mamas raised us right. At Slick's we believe that hard work and smart work are high on the totem pole of customer service but above all sits honest work. Starting from John "Slick" Doe all the way down to our newest mechanics and specialist, you can rest assure that the services you get from Slick's will be not only the best you can find, but the most honest as well. We'll never try to force you into making unnecessary purchases or repairs, and we'll always speak with you before we do anything that wasn't originally agreed upon. That's the Slick's promise and our word is our most valuable possession.
             </p>
             <a href="http://julioaenlle.net/slicksautobody/index.php/contact/">MAKE AN APPOINTMENT</a>
         </div>
-        <div class="two columns blank">Black Column</div>
         <div class="twelve columns maindeals">
             <ul>
                 <li><div class="blocks hm1"><p>Prevention</p></div></li>
